456ce72cf2cac40a467169f62d466f5234effefe
rwxp <sdrivert@hotmail.com>
2023-10-25 22:58:52 -0500

    Database connection added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from "express";
 import graphqlHTTP from "express-graphql";
 import schema from "./schema.js";
+import { connectDB } from "../config/db.js";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+// Env variables
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
 
 const app = express();
+
+//BodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Graphql middleware
 app.use(
@@ -13,6 +25,8 @@ app.use(
   })
 );
 
-app.listen(3000, () => {
-  console.log("Server on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
 });
